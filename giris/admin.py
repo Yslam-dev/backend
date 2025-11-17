ac7d09b25974453f2b4cd98b2d8d505b59a633b4
Yslam-dev <yslam0791@gmail.com>
2025-11-17 22:17:06 +0500

    Update admin.py

--- giris/admin.py
+++ giris/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.hashers import make_password
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .utils import generate_password
 
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
     fieldsets = (
         (None, {'fields': ('username', 'surname', 'password')}),
         ('Permissions', {'fields': ('role', 'group_number', 'is_staff', 'is_active')}),
@@ -17,14 +22,11 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
+    # Генерация пароля
     def save_model(self, request, obj, form, change):
-        # Если создаётся новый пользователь — генерируем пароль
         if not change:
             password = generate_password(6)
             obj.password = make_password(password)
-            obj.generated_password = password
-            print("Generated password:", password)  # видно в консоли сервера
-
         super().save_model(request, obj, form, change)
 
 admin.site.register(User, CustomUserAdmin)
