ef2d35238e47641aa8565b99c5814b35192c3871
Yslam-dev <yslam0791@gmail.com>
2025-11-27 22:17:03 +0500

    Update urls.py

--- giris/urls.py
+++ giris/urls.py
@@ -3,13 +3,13 @@ from .views import api_login_jwt, user_info_api, api_create_user, api_list_users
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/login_jwt/', api_login_jwt, name='api-login-jwt'),
-    path('api/user/', user_info_api, name='user-info'),
+    path('login_jwt/', api_login_jwt, name='api-login-jwt'),
+    path('user/', user_info_api, name='user-info'),
     
     # Yeni eklenenler:
-    path('api/create-user/', api_create_user, name='api-create-user'),
-    path('api/list-users/', api_list_users, name='api-list-users'),
+    path('create-user/', api_create_user, name='api-create-user'),
+    path('list-users/', api_list_users, name='api-list-users'),
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
