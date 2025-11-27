7ebb679f868c83ff5bcab25e961b73f0c7e88e2e
Yslam-dev <yslam0791@gmail.com>
2025-11-27 22:16:10 +0500

    Update urls.py

--- onlayn_test/urls.py
+++ onlayn_test/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('giris.urls')),
+    path('api/', include('giris.urls')),
     path('api/tests/', include('tests.urls')),
 ]
